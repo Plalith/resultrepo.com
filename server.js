775118ce4e4a835081f25acad9d9ba795547ab2d
Lalith Kumar <lalith889@gmail.com>
2019-04-20 11:01:29 +0530

    changed port 1000 to 80

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.get('/*', (req, res) => {
 });
 
 // Set Port
-const port = process.env.PORT || '1000';
+const port = process.env.PORT || '80';
 app.set('port', port);
 
 const server = http.createServer(app);
